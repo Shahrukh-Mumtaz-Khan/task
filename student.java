fa7deae87a7169c27a0b0cea544a59ddbae85552
Shahrukh-Mumtaz-Khan <113114955+Shahrukh-Mumtaz-Khan@users.noreply.github.com>
2022-09-15 16:03:06 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+abstract class student{
+ abstract void rollno();
+
+}
+class skand extends student{
+	void rollno(){
+		System.out.print("3626532");
+	}
+	 public static void main(String[] args) {
+		 skand s=new skand();
+		 s.rollno(); 
+		  
+	}
+}
